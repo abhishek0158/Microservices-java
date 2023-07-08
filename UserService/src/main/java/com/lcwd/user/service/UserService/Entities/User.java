9a16c1b07bb885ef6c7b3d515c1822f016d99d68
abhishek0158 <gkrabhishek21@gmail.com>
2023-07-08 13:28:40 +0530

    completed UserService microservice

--- UserService/src/main/java/com/lcwd/user/service/UserService/Entities/User.java
+++ UserService/src/main/java/com/lcwd/user/service/UserService/Entities/User.java
@@ -4,10 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -24,5 +23,7 @@ public class User {
     private String about;
 
 
+    @Transient//@Transient allows not store in database
+    private List<Rating> ratings=new ArrayList<>();
 
 }
